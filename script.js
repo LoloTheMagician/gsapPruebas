954e64acd19c33482b22f6d8c7bab164d8cda1c9
Lolo <lologameprogrammer@gmail.com>
2024-02-17 22:07:26 +0100

    It works!

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+import { gsap } from './node_modules/gsap/index.js'
 
+gsap.to('.box', {
+    x : 100,
+    rotation : 45,
+    scale: .75, 
+    duration: .75,
+    borderRadius: 50,
+    repeat: 1,
+    yoyo: true
+})
