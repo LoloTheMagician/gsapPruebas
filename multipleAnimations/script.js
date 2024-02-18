f282f1cfc6227573e359c6fc38610b48c367ed9b
Lolo <lologameprogrammer@gmail.com>
2024-02-18 11:25:40 +0100

    Function to create random colors for the circles

--- multipleAnimations/script.js
+++ multipleAnimations/script.js
@@ -1,9 +1,29 @@
 import { gsap } from '../node_modules/gsap/index.js'
 
-gsap.from('.circle', {
-    y: () => 'random(-200,200)',
-    opacity: 0,
+const COLORS = {
+    BLUE: '#08A',
+    RED: '#d21',
+    GREEN: '#08A045',
+    VIOLET: '#8000AA'
+}
+
+const randomColor = () => {
+    const colorsLen = Object.keys(COLORS).length
+    const color = Math.floor(Math.random() * colorsLen)
+    return COLORS[
+        Object.keys(COLORS)[color]
+    ]
+}
+
+
+gsap.to('.circle', {
+    y: 100,
+    backgroundColor: () => randomColor(),
+    // opacity: 0,
+
+    duration: 2,
     stagger: .25,
-    // repeat: Infinity,
+    ease: 'elastic',
+    repeat: Infinity,
     yoyo: true
 })
