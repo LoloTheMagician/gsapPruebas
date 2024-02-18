95b18f4927614bf2716b0c373a743cab320bb5e6
Lolo <lologameprogrammer@gmail.com>
2024-02-18 12:24:13 +0100

    Start doing Timelines

--- timelines/script.js
+++ timelines/script.js
@@ -1,24 +1,23 @@
-import { gsap } from "../node_modules/gsap/index.js";
 
-const divs = ['rectangle', 'square']
+// const divs = ['rectangle', 'square']
 
-gsap.from(divs.map(div => '.' + div), {
-    y: 100,
-    x: 100,
-    rotation: 'random(0, 365)',
-    opacity : 0,
-    width: () => divs.map(div => {
-        const divDom = 
-        document.getElementsByClassName(div)
-        const divWidth = divDom[0].offsetWidth
-        return divWidth + 50
-    }).at(0),
-    height: () => divs.map(div => {
-        const divDom = 
-        document.getElementsByClassName(div)
-        const divWidth = divDom[0].offsetHeight
-        return divWidth + 50
-    }).at(0),
+// gsap.from(divs.map(div => '.' + div), {
+//     y: 100,
+//     x: 100,
+//     rotation: 'random(0, 365)',
+//     opacity : 0,
+//     width: () => divs.map(div => {
+//         const divDom = 
+//         document.getElementsByClassName(div)
+//         const divWidth = divDom[0].offsetWidth
+//         return divWidth + 50
+//     }).at(0),
+//     height: () => divs.map(div => {
+//         const divDom = 
+//         document.getElementsByClassName(div)
+//         const divWidth = divDom[0].offsetHeight
+//         return divWidth + 50
+//     }).at(0),
 
-    duration : 2,
-})
+//     duration : 2,
+// })
