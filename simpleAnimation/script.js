b5ec9eee26d9f135952868eb4095ae755288207c
Lolo <lologameprogrammer@gmail.com>
2024-02-18 09:57:15 +0100

    First Animation created on its own folder

--- simpleAnimation/script.js
+++ simpleAnimation/script.js
@@ -1,11 +1,13 @@
-import { gsap } from './node_modules/gsap/index.js'
+import { gsap } from '../node_modules/gsap/index.js'
 
 gsap.to('.box', {
+    //props
     x : 100,
     rotation : 45,
     scale: .75, 
     duration: .75,
     borderRadius: 50,
+    
     repeat: 1,
-    yoyo: true
+    yoyo: true,
 })
